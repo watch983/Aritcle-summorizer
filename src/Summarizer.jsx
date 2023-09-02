62000c9159e202d24029674a4efe67c1b872e44e
watch983 <139033861+watch983@users.noreply.github.com>
2023-09-02 14:56:44 +0530

    1 file modified

--- src/Summarizer.jsx
+++ src/Summarizer.jsx
@@ -1,3 +1,25 @@
+const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
+  params: {
+    url: 'https://redux-toolkit.js.org/rtk-query/overview',
+    length: '3',
+  },
+  headers: {
+    'X-RapidAPI-Key': 'd99907482cmsh9ab0fc66bc0daf3p153337jsn95f51e31894e',
+    'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com',
+  },
+};
+
+try {
+  const response = await axios.request(options);
+  console.log(response.data);
+} catch (error) {
+  console.error(error);
+}
+
 const Summarizer = () => {
   return <div>hello</div>;
 };
